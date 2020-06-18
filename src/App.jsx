f432ae8f524f30f6523dfbbc4d40eb323106efe4
whyang115 <whyang115@gmail.com>
2020-06-18 14:33:25 +0800

    feat: adjust upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import React from "react";
-import axios from "axios";
-import { Base64 } from "js-base64";
-import styled from "styled-components";
-import { get } from "lodash";
-import cx from "classnames";
+import React from 'react';
+import axios from 'axios';
+import { Base64 } from 'js-base64';
+import styled from 'styled-components';
+import { get } from 'lodash';
+import cx from 'classnames';
 // 引入编辑器组件
-import BraftEditor from "braft-editor";
+import BraftEditor from 'braft-editor';
 // 引入编辑器样式
-import "braft-editor/dist/index.css";
-import "./App.css";
-
+import 'braft-editor/dist/index.css';
+import './App.css';
 const AppWrap = styled.div`
   height: 100%;
   .previewWrap {
@@ -34,32 +33,29 @@ const AppWrap = styled.div`
   }
 `;
 
-const TOKEN =
-  "QCLMePM7TGMVvirxL32Y9696IlLrdz4OsGRlRoWB:mia__LDQq3pz6TTVVV-R8AWZoUg=:eyJzY29wZSI6Im1kcGljIiwicmV0dXJuQm9keSI6IntcImtleVwiOlwiJChrZXkpXCIsXCJldGFnXCI6XCIkKGV0YWcpXCIsXCJidWNrZXRcIjpcIiQoYnVja2V0KVwiLFwiZnNpemVcIjpcIiQoZnNpemUpXCIsXCJzZXJ2ZXJOYW1lXCI6XCIkKHg6c2VydmVyTmFtZSlcIixcImZpbGVQYXRoXCI6XCIkKHg6ZmlsZVBhdGgpXCIsXCJmaWxlTmFtZVwiOlwiJCh4OmZpbGVOYW1lKVwiLFwib3JpZ2luYWxGaWxlTmFtZVwiOlwiJCh4Om9yaWdpbmFsRmlsZU5hbWUpXCIsXCJmaWxlRXh0XCI6XCIkKHg6ZmlsZUV4dClcIn0iLCJkZWFkbGluZSI6MTU3NDE3OTE5OSwiaW5zZXJ0T25seSI6MCwiZGV0ZWN0TWltZSI6MCwiZnNpemVMaW1pdCI6MH0=";
-
 export default class EditorDemo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       // 创建一个空的editorState作为初始值
       editorState: BraftEditor.createEditorState(null),
-      token: "",
-      uploadServer: "https://upload.qiniup.com/putb64/-1/key/upload/",
-      previewServer:"https://pic.mingdao.com/",
-      key: "",
+      token: '',
+      uploadServer: 'https://upload.qiniup.com/putb64/-1/key/upload/',
+      previewServer: 'https://pic.mingdao.com/',
+      key: '',
       preview: true,
     };
-    window.getPara = (para) => {
-      const {html,...rest} =JSON.parse( Base64.decode(para))
+    window.getPara = para => {
+      const { html, ...rest } = JSON.parse(Base64.decode(para));
       window.richTextHtml = html;
       this.setState({
         editorState: BraftEditor.createEditorState(html),
-       ...rest 
+        ...rest,
       });
     };
-    window.switchPreview = (str) => {
-      this.setState({ preview: str === "true" ? true : false });
-      if (str === "false") {
+    window.switchPreview = str => {
+      this.setState({ preview: str === 'true' ? true : false });
+      if (str === 'false') {
         this.computeBottomPadding();
       }
     };
@@ -71,10 +67,10 @@ export default class EditorDemo extends React.Component {
   }
 
   computeBottomPadding = () => {
-    const $editor = document.querySelector(".braftEditorWrap ");
-    const $control = document.querySelector(".bf-controlbar");
+    const $editor = document.querySelector('.braftEditorWrap ');
+    const $control = document.querySelector('.bf-controlbar');
     if ($editor && $control) {
-      $editor.style.paddingBottom = $control.offsetHeight + 10 + "px";
+      $editor.style.paddingBottom = $control.offsetHeight + 10 + 'px';
     }
   };
   randomStr = () => Math.random().toString(16).slice(2);
@@ -83,28 +79,30 @@ export default class EditorDemo extends React.Component {
     const { uploadServer, token, key } = this.state;
     const reader = new FileReader();
     reader.readAsDataURL(file);
-    reader.addEventListener("load", (data) => {
-      const result = get(data, ["target", "result"]);
-      const type = file.type.split("/")[1];
-      const defaultKey = Base64.encode(`${this.randomStr()}.${type}`);
-      const wholeUrl = `${uploadServer}${key || defaultKey}`;
-      axios
-        .post(wholeUrl, result.split(",")[1], {
-          headers: {
-            "Content-Type": "application/octet-stream",
-            Authorization: `UpToken ${token || TOKEN}`,
-          },
-          onUploadProgress: function (data) {
-            progress && progress((data.loaded / data.total) * 100);
-          },
-        })
+    reader.addEventListener('load', data => {
+      const result = get(data, ['target', 'result']);
+      const type = file.type.split('/')[1];
+      const fileData = result.split(',')[1];
+      const fileName = Base64.encode(`${this.randomStr()}.${type}`);
+      const formData = new FormData();
+      formData.append('key', `${key}${fileName}.${type}`);
+      formData.append('token', token);
+      formData.append('file', fileData);
+      axios({
+        url: uploadServer,
+        method: 'POST',
+        data: formData,
+        onUploadProgress: function (data) {
+          progress && progress((data.loaded / data.total) * 100);
+        },
+      })
         .then(({ data }) => {
-          const {previewServer} = this.props
-          const { key = "" } = data || {};
-          const url = `${previewServer}${key}`;
+          const { previewServer } = this.state;
+          const { key = '' } = data || {};
+          const url = `${previewServer}/${key}`;
           success({ url });
         })
-        .catch((err) => {
+        .catch(err => {
           error(err);
         });
     });
@@ -118,36 +116,33 @@ export default class EditorDemo extends React.Component {
   //   }
   // };
 
-  handleEditorChange = (editorState) => {
+  handleEditorChange = editorState => {
     this.setState({ editorState });
     window.richTextHtml = editorState.toHTML();
   };
 
   render() {
     const controls = [
-      "headings",
-      "text-color",
-      "bold",
-      "italic",
-      "underline",
-      "strike-through",
-      "separator",
-      "list-ul",
-      "list-ol",
-      { key: "media", title: "添加图片", text: "添加图片" },
-      "separator",
-      "undo",
-      "redo",
+      'headings',
+      'text-color',
+      'bold',
+      'italic',
+      'underline',
+      'strike-through',
+      'separator',
+      'list-ul',
+      'list-ol',
+      { key: 'media', title: '添加图片', text: '添加图片' },
+      'separator',
+      'undo',
+      'redo',
     ];
     const { editorState, preview } = this.state;
     return (
-      <AppWrap className={cx("App", "preview")}>
+      <AppWrap className={cx('App', 'preview')}>
         {preview ? (
           <div className="previewWrap">
-            <div
-              className="previewContent"
-              dangerouslySetInnerHTML={{ __html: editorState.toHTML() }}
-            ></div>
+            <div className="previewContent" dangerouslySetInnerHTML={{ __html: editorState.toHTML() }}></div>
           </div>
         ) : (
           <BraftEditor
