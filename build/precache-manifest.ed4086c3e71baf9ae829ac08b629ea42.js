0e78f08ac38ee8188e9241ebf3085a1184f10199
whyang115 <whyang115@gmail.com>
2020-06-18 10:08:48 +0800

    feat: build

---
+++ build/precache-manifest.ed4086c3e71baf9ae829ac08b629ea42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f4fdd55d38add517437340618b4283",
+    "url": ".index.html"
+  },
+  {
+    "revision": "d58eb47fb731c0073068",
+    "url": ".static/css/2.54d918f9.chunk.css"
+  },
+  {
+    "revision": "8aa53e3e49ac7a89daea",
+    "url": ".static/css/main.226a7976.chunk.css"
+  },
+  {
+    "revision": "d58eb47fb731c0073068",
+    "url": ".static/js/2.f4dc5876.chunk.js"
+  },
+  {
+    "revision": "8aa53e3e49ac7a89daea",
+    "url": ".static/js/main.53ad8f16.chunk.js"
+  },
+  {
+    "revision": "c4ad0b602f3e2f7126a0",
+    "url": ".static/js/runtime-main.11c6e384.js"
+  }
+]);
